7237ff341f7e15a3b4488fb58f33dabe59bfa64a
Ademar <ademarneto1202@gmail.com>
2017-12-19 08:27:50 -0300

    Atualização

--- crud-bootstrap-php/captcha.php
+++ crud-bootstrap-php/captcha.php
@@ -5,6 +5,9 @@
 // Criar o texto para o captcha.
  $codigoCaptcha = substr(md5( time()) ,0,9);
 
+// Guardar o texto numa variável session
+ $_SESSION['captcha'] = $codigoCaptcha;
+
 // Criar um novo recurso de imagem a partir de um arquivo.
  $imagemCaptcha = imagecreatefrompng("fundocaptcha.png");
  
